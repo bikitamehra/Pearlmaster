33fca25320a78beb1ac6e825164890a36471b0bd
bikitamehra <126053309+bikitamehra@users.noreply.github.com>
2024-05-22 22:53:28 +0530

    my first commi

---
+++ src/basic/MyFirstprogram.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class MyFirstprogram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello good morning, my name is bikita");
+
+	}
+
+}
---
+++ src/basic/comment.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class comment {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+	int age=18;	
+String message =(age >= 18)? " You are eligible to Vote " :  " You are Not eligible to Vote ";
+		//System.out.println(Message);
+		
+	}
+
+}
